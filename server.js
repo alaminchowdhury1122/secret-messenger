434e55ac19c7cd326beb6618baa8694b15e60ef3
alaminchowdhury1122 <alonealamin109@gmail.com>
2025-03-26 22:55:21 +0600

    Add message storage with lowdb

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const { Low } = require('lowdb');
+const { JSONFile } = require('lowdb/node'); // For Node.js environment
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+// Setup lowdb
+const file = 'db.json'; // Database file
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+// Initialize database with default structure
+async function initDb() {
+    await db.read();
+    db.data ||= { messages: [] }; // If no data, initialize with empty messages array
+    await db.write();
+}
+initDb();
 
 // Hardcoded users
 const users = { "Bro": "pass123", "GF": "love456" };
@@ -12,12 +27,17 @@ let connectedUsers = [];
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.on('connection', (socket) => {
-    socket.on('login', ({ username, password }) => {
+    socket.on('login', async ({ username, password }) => {
         if (users[username] && users[username] === password && connectedUsers.length < 2) {
             if (!connectedUsers.includes(username)) {
                 connectedUsers.push(username);
                 socket.username = username;
                 socket.emit('login_success', username);
+
+                // Send chat history to the user
+                await db.read();
+                const messages = db.data.messages;
+                socket.emit('chat_history', messages);
             } else {
                 socket.emit('login_fail', 'Already logged in!');
             }
@@ -26,8 +46,14 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('chat message', (data) => {
+    socket.on('chat message', async (data) => {
         if (socket.username) {
+            // Store message in database
+            await db.read();
+            db.data.messages.push({ user: socket.username, msg: data.msg });
+            await db.write();
+
+            // Broadcast message to all users
             io.emit('chat message', { user: socket.username, msg: data.msg });
         }
     });
